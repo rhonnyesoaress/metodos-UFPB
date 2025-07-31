0061fb925777130aa484ad6bb81d3d72e7028149
Davi Barros <davih.tbarros@gmail.com>
2025-07-31 09:37:26 -0300

    exceptions adicionadas
    falta verificar certinho

--- Manager/user_manager.py
+++ Manager/user_manager.py
@@ -3,19 +3,25 @@ from Entidades.user import User
 
 class UserManager:
     def __init__(self):
-        self.users = {}  # dict com username -> User
+        self.users = []  # dict com username -> User
 
-    def register_user(self, username: str, password: str) -> bool:
-        if username in self.users:
-            return False  # Já existe
-        self.users[username] = User(username, password)
-        return True
 
-    def login(self, username: str, password: str) -> bool:
-        user = self.users.get(username)
-        if not user:
-            return False
-        return user.check_password(password)
-    
+    def register_user(self, username, password):
+        if any(user.username == username for user in self.users):
+            raise ValueError("Usuário já existe.")
+        self.users.append(User(username, password))
+
+
+    def login(self, username, password):
+        for user in self.users:
+            if user.username == username:
+                if user.check_password(password):
+                    return True
+                else:
+                    raise ValueError("Senha incorreta.")
+        raise ValueError("Usuário não encontrado.")
+
+
+
     def listar_usuarios(self):
-        return list(self.users.keys())
+        return [user.username for user in self.users]
